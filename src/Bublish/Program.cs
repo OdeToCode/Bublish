990c598027eb243636e602d0a3bb6efd824c1011
Scott Allen <scott@OdeToCode.com>
2018-08-07 19:34:22 -0400

    initial structure

---
+++ src/Bublish/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bublish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
